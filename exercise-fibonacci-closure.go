3932450cc69d7f781774e0c9bdb240a3a052e8f7
callofdutyops <awsomekde@gmail.com>
2019-03-12 13:42:20 +0800

    finished part4

---
+++ exercise-fibonacci-closure.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a, b, cur := 0, 1, 0
+	return func() int {
+        a, b, cur = b, a+b, a
+        return cur
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
