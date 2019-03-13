3c9e14314331c52d8e880dc755a82c2a3a61701c
callofdutyops <awsomekde@gmail.com>
2019-03-13 13:49:15 +0800

    method

--- exercise-fibonacci-closure.go
+++ exercise-fibonacci-closure.go
@@ -5,8 +5,8 @@ import "fmt"
 func fibonacci() func() int {
 	a, b, cur := 0, 1, 0
 	return func() int {
-        a, b, cur = b, a+b, a
-        return cur
+		a, b, cur = b, a+b, a
+		return cur
 	}
 }
 
